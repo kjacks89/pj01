c11a3299ab9b8dca458b7c045d56e6dc974e2d43
Kyle <kjacks89@gmail.com>
2018-02-09 16:14:50 -0500

    all but permissions tested

--- www/web_server.py
+++ www/web_server.py
@@ -2,19 +2,15 @@ import socket
 import threading
 import argparse
 import sys
-from pathlib import Path
 import os
 
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 47656 # My available ports are 47656 or 47657
 SERVER_ADDR = (SERVER_HOST, SERVER_PORT)
-path = Path('./www')
 parser = argparse.ArgumentParser()
-parser.add_argument("--root")
+parser.add_argument('--root', '-R', metavar = 'PORT', default = os.getcwd())
 args = parser.parse_args()
-if args.root:
-	path = Path(args.root)
-
+SERVER_ROOT = args.root
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -32,44 +28,50 @@ class HandlerThread(threading.Thread):
 				self.address = address
 
 		def run(self):
-				data = client.recv(1024).decode()
+				finalHeader = b''
+				data = client.recv(1024).decode().strip()
 				file_requested = data.split(' ')[1]
 				if file_requested == "/":
-						file_requested = "/index.html"
-				pathToFile = path / file_requested
-				httptype = data.split(' ')[2]
-				if file_requested.endswith(".html") == -1 or file_requested.endswith(".txt") == -1 or file_requested.endswith(".png") == -1:
-					if httpType == 'HTTP/1.0':
-						self.client.send(b'HTTP/1.0 400 Bad Request')
-					else:
-						self.client.send(b'HTTP/1.1 400 Bad Request')
-				if os.path.isfile(pathToFile):
-					try:
-						f = open(pathToFile, 'rb')
-						responseBody = f.read()
-						f.close()
-						contentType = b''
-						if file_requested.endswith("html"):
-							contentType = b"text/html"
-						elif file_requested.endswith("png"):
-							contentType = b"image/png"
-						else:
-							contentType = b"text/plain"
-						if httpType == 'HTTP/1.0':
-							self.client.send(b'HTTP/1.0 200 OK\r\n' + contentType + '\r\n' + responseBody)
-						else:
-							self.client.send(b'HTTP/1.1 200 OK\r\n' + contentType + '\r\n' + responseBody)
-					except Exception as e:
-						if httpType == 'HTTP/1.0':
-							self.client.send(b'HTTP/1.0 403 Forbidden')
-						else:
-							self.client.send(b'HTTP/1.1 403 Forbidden')
+					file_requested += 'index.html'
+				httpType = data.split(' ')[2]
+				if file_requested.endswith(".html") == 0 and file_requested.endswith(".txt") == 0 and file_requested.endswith(".png") == 0:
+					finalHeader = b'400 Bad Request'
+					self.client.send(finalHeader)
 				else:
-					if httpType == 'HTTP/1.0':
-						self.client.send(b'HTTP/1.0 404 Not Found')
+					responseCode = b''
+					responseBody = b''
+					finalHeader = b''
+					path = SERVER_ROOT + file_requested
+					if os.path.isfile(path):
+						try:
+							f = open(path, 'rb')
+							responseBody = f.read()
+							f.close()
+							contentType = b''
+							if file_requested.endswith("html"):
+								contentType = b'text/html'
+							elif file_requested.endswith("png"):
+								contentType = b'image/png'
+							else:
+								contentType = b'text/plain'
+							if httpType == 'HTTP/1.0':
+								responseCode = b'HTTP/1.0 200 OK'
+								finalHeader = responseCode + b'\r\n' + contentType + b'\r\n\r\n' + responseBody
+								self.client.send(finalHeader)
+							else:
+								responseCode = b'HTTP/1.1 200 OK'
+								finalHeader = responseCode + b'\r\n' + contentType + b'\r\n\r\n' + responseBody
+								self.client.send(finalHeader)
+						except Exception as e:
+							if httpType == 'HTTP/1.0':
+								self.client.send(b'HTTP/1.0 403 Forbidden\r\n\r\n')
+							else:
+								self.client.send(b'HTTP/1.1 403 Forbidden\r\n\r\n')
 					else:
-						self.client.send(b'HTTP/1.1 404 Not Found')
-								
+						finalHeader = b'404 Not Found\r\n\r\n'
+						self.client.send(finalHeader)
+					#self.client.shutdown(socket.SHUT_RDWR)
+				self.client.close()					
 while True:
 		#Accept Connection
 		client, address = connection.accept()
